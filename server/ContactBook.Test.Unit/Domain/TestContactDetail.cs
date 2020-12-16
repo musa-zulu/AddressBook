ecd1ac33f5cbba8c31ab0c973fa5c2cb5d351971
Musa <zuluchs@gmail.com>
2020-12-16 21:26:30 +0200

    TestContactDetails
    Added tests to verify that ContactDetails have correct properties

---
+++ server/ContactBook.Test.Unit/Domain/TestContactDetail.cs
@@ -0,0 +1,40 @@
+﻿using AddressBook.Domain.Entities;
+using NUnit.Framework;
+using PeanutButter.TestUtils.Generic;
+using System;
+
+namespace AddressBook.Test.Unit.Domain
+{
+    [TestFixture]
+    public class TestContactDetail
+    {
+        [Test]
+        public void Construct()
+        {
+            //---------------Set up test pack-------------------
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            Assert.DoesNotThrow(() => new ContactDetail());
+            //---------------Test Result -----------------------
+        }
+
+        [TestCase("ContactDetailId", typeof(int))]
+        [TestCase("Description", typeof(string))]
+        [TestCase("ContactTypeId", typeof(ContactType))]
+        [TestCase("ContactId", typeof(int))]
+        public void ContactDetail_ShouldHaveProperty(string propertyName, Type propertyType)
+        {
+            //---------------Set up test pack-------------------
+            var sut = typeof(ContactDetail);
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            sut.ShouldHaveProperty(propertyName, propertyType);
+
+            //---------------Test Result -----------------------
+        }
+    }
+}
