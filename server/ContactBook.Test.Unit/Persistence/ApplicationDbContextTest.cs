c2b141dfc3c60c7730a4481ed96b97415d0dc7c8
Musa <zuluchs@gmail.com>
2020-12-16 21:16:20 +0200

    updated tests for db context add, remove and update

--- server/ContactBook.Test.Unit/Persistence/ApplicationDbContextTest.cs
+++ server/ContactBook.Test.Unit/Persistence/ApplicationDbContextTest.cs
@@ -10,10 +10,46 @@ namespace AddressBook.Test.Unit.Persistence
         [Test]
         public void CanInsertContactIntoDatabasee()
         {
+            //---------------Set up test pack-------------------
             using var context = new ApplicationDbContext();
             var contact = new Contact();
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
             context.Contacts.Add(contact);
+            //---------------Test Result -----------------------
             Assert.AreEqual(EntityState.Added, context.Entry(contact).State);
+        }
+
+        [Test]
+        public void CanUpdateContactIntoDatabasee()
+        {
+            //---------------Set up test pack-------------------
+            using var context = new ApplicationDbContext();
+            var contact = new Contact
+            {
+                ContactId = 1
+            };
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            context.Contacts.Update(contact);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(EntityState.Modified, context.Entry(contact).State);
+        }
+
+        [Test]
+        public void CanDeleteContactIntoDatabasee()
+        {
+            //---------------Set up test pack-------------------
+            using var context = new ApplicationDbContext();
+            var contact = new Contact
+            {
+                ContactId = 1
+            };
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            context.Contacts.Remove(contact);
+            //---------------Test Result -----------------------            
+            Assert.AreEqual(EntityState.Deleted, context.Entry(contact).State);
         }
     }
 }
