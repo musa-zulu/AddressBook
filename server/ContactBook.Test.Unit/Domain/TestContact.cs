c082c145562ca64e1a5eeae13d5b9004c9853d9f
Musa <zuluchs@gmail.com>
2020-12-16 21:17:29 +0200

    TestContact Poco
    Added tests for contact class to verify if it has correct properties

---
+++ server/ContactBook.Test.Unit/Domain/TestContact.cs
@@ -0,0 +1,43 @@
+﻿using AddressBook.Domain.Entities;
+using NUnit.Framework;
+using PeanutButter.TestUtils.Generic;
+using System;
+using System.Collections.Generic;
+
+namespace AddressBook.Test.Unit.Domain
+{
+    [TestFixture]
+    public class TestContact
+    {
+        [Test]
+        public void Construct()
+        {
+            //---------------Set up test pack-------------------
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            Assert.DoesNotThrow(() => new Contact());
+            //---------------Test Result -----------------------
+        }
+
+        [TestCase("ContactId", typeof(int))]
+        [TestCase("FirstName", typeof(string))]      
+        [TestCase("Surname", typeof(string))]
+        [TestCase("BirthDate", typeof(DateTime))]
+        [TestCase("UpdatedDate", typeof(DateTime))]
+        [TestCase("ContactDetails", typeof(List<ContactDetail>))]
+        public void Contact_ShouldHaveProperty(string propertyName, Type propertyType)
+        {
+            //---------------Set up test pack-------------------
+            var sut = typeof(Contact);
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            sut.ShouldHaveProperty(propertyName, propertyType);
+
+            //---------------Test Result -----------------------
+        }
+    }
+}
