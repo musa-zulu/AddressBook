fc6642e07294800f9d760ba974661cc5bd621d1f
Musa <zuluchs@gmail.com>
2021-01-13 11:31:54 +0200

    added useCors

--- server/ContactBook/Startup.cs
+++ server/ContactBook/Startup.cs
@@ -63,6 +63,9 @@ namespace ContactBook
 
             log.AddSerilog();
 
+            app.UseCors(options =>
+               options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
             RunMigrations(service);
 
             app.UseEndpoints(endpoints =>
